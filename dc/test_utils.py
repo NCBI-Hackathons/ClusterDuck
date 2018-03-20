430922a87413c5c505e155b6cca7a207f4a7299c
Yi Hsiao <hsiaoyi0504@gmail.com>
2018-03-19 21:03:28 -0400

    move get_abstracts.py into dc/utils.py and add test case

--- dc/test_utils.py
+++ dc/test_utils.py
@@ -1,3 +1,10 @@
 from utils import get_pubmed_ids_from_phenotypes, get_pubmed_ids_from_rsids, get_rsids_from_pubmed_id
 import unittest
 
+class test_get_pubmed_ids_from_rsids(unittest.TestCase):
+    def test(self):
+        rsids = ['7041', '4588']
+        self.assertEqual(len(get_pubmed_ids_from_rsids(rsids)), 200)
+
+if __name__ == '__main__':
+    unittest.main()

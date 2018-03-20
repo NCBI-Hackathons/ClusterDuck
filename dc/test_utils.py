098ef77063163c28b5c77343df007016c274175b
Ubuntu <ubuntu@ip-172-16-243-138.ec2.internal>
2018-03-20 13:08:56 +0000

    add get_pubmed_ids_from_phenotypes() function

--- dc/test_utils.py
+++ dc/test_utils.py
@@ -1,15 +1,24 @@
 from utils import get_pubmed_ids_from_phenotypes, get_pubmed_ids_from_rsids, get_rsids_from_pubmed_id
 import unittest
 
+
 class test_get_pubmed_ids_from_rsids(unittest.TestCase):
     def test(self):
         rsids = ['7041', '4588']
         self.assertEqual(len(get_pubmed_ids_from_rsids(rsids)), 40)
 
+
 class test_get_rsids_from_pubmed_id(unittest.TestCase):
     def test(self):
         pubmed_id = '25626708'
         self.assertEqual(len(get_rsids_from_pubmed_id(pubmed_id)), 29)
+
+
+class test_get_pubmed_ids_from_phenotypes(unittest.TestCase):
+    def tests(self):
+        phenotypes = ['Neoplasm', 'Growth abnormality']  # HP:0002664 and HP:0001507
+        self.assertEqual(len(get_pubmed_ids_from_phenotypes(phenotypes)), 40)
+
 
 if __name__ == '__main__':
     unittest.main()
--- dc/utils.py
+++ dc/utils.py
@@ -7,7 +7,13 @@ Entrez.email = "trivneel211@gmail.com"
 
 def get_pubmed_ids_from_phenotypes(phenotypes):
     pubmed_ids = []
-    # TODO
+    for phen in phenotypes:
+        query = phen
+        handle = Entrez.esearch(db='pubmed', term=query, rettype='medline', retmode='text')
+        record = Entrez.read(handle)
+        handle.close()
+        idList = record['IdList']
+        pubmed_ids.extend(idList)
     return pubmed_ids
 
 
@@ -27,3 +33,4 @@ def get_rsids_from_pubmed_id(pubmed_id):
     result = Entrez.read(Entrez.elink(dbfrom='pubmed', db='snp', linkname='pubmed_snp_cited', id=pubmed_id))
     rsids = [r['Id'] for r in result[0]['LinkSetDb'][0]['Link']]
     return rsids
+

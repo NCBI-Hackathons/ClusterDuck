09420974d0f98f70b84e9e40d74c307718e31f37
lwgray <lwgray@gmail.com>
2018-03-20 16:39:44 -0400

    Fix hypenated words

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,6 @@
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
 
 STOPWORDS = set(stopwords.words('english'))
 
@@ -12,9 +11,7 @@ def preprocess(abstract):
     abstract = abstract.lower()
 
     # tokenize words, remove punctuation and stopwords, lemmatize
-    tokenizer = RegexpTokenizer(r'[a-zA-Z]\w+\'?\w*|\-\w+')
-    # tokenizer = RegexpTokenizer(r'[\w-]')
+    tokenizer = RegexpTokenizer(r'\w[\w-]+')
     tokens = tokenizer.tokenize(abstract)
-    # tokens = word_tokenize(abstract)
     words = [wnl.lemmatize(word) for word in tokens if word not in STOPWORDS]
     return words

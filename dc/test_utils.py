a288d42f1d7805ffd484ff5a30b001c3ff7ada1c
Ubuntu <ubuntu@ip-172-16-243-138.ec2.internal>
2018-03-20 13:32:10 +0000

    type checking in test cases

--- dc/test_utils.py
+++ dc/test_utils.py
@@ -5,19 +5,31 @@ import unittest
 class test_get_pubmed_ids_from_rsids(unittest.TestCase):
     def test(self):
         rsids = ['7041', '4588']
-        self.assertEqual(len(get_pubmed_ids_from_rsids(rsids)), 40)
+        pubmed_ids = get_pubmed_ids_from_rsids(rsids)
+        self.assertEqual(len(pubmed_ids), 40)
+        self.assertIsInstance(pubmed_ids, list)
+        for id in pubmed_ids:
+            self.assertIsInstance(id, str)
 
 
 class test_get_rsids_from_pubmed_id(unittest.TestCase):
     def test(self):
         pubmed_id = '25626708'
-        self.assertEqual(len(get_rsids_from_pubmed_id(pubmed_id)), 29)
+        rsids = get_rsids_from_pubmed_id(pubmed_id)
+        self.assertEqual(len(rsids), 29)
+        self.assertIsInstance(rsids, list)
+        for id in rsids:
+            self.assertIsInstance(id, str)
 
 
 class test_get_pubmed_ids_from_phenotypes(unittest.TestCase):
     def tests(self):
         phenotypes = ['Neoplasm', 'Growth abnormality']  # HP:0002664 and HP:0001507
-        self.assertEqual(len(get_pubmed_ids_from_phenotypes(phenotypes)), 40)
+        pubmed_ids = get_pubmed_ids_from_phenotypes(phenotypes)
+        self.assertEqual(len(pubmed_ids), 40)
+        self.assertIsInstance(pubmed_ids, list)
+        for id in pubmed_ids:
+            self.assertIsInstance(id, str)
 
 
 if __name__ == '__main__':
